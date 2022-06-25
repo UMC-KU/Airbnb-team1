a1aa1e7050e77ced429dc8c7dbf88e863c6367d1
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-06-26 02:59:10 +0900

    유경종/ FIX /members > /member | unused model 객체 삭제, welcome page 구현

--- server/airbnb_server/src/main/java/com/example/demo/src/member/MemberService.java
+++ server/airbnb_server/src/main/java/com/example/demo/src/member/MemberService.java
@@ -29,45 +29,4 @@ public class MemberService {
         this.jwtService = jwtService;
     }
 
-    //ㅇㅇㅁㄹㅇㄴㄹdd
-
-//    public PostMemberRes createUser(PostMemberReq postMemberReq) throws BaseException {
-//        // 이메일 중복 확인 // 의미적 validation 처리
-//        if(memberProvider.checkEmail(postMemberReq.getEmail()) == 1){
-//            // Dao 아닌 Provider 에서. Why?
-//            // 무언가를 check하는 것 = 조회의 의미. 따라서 Provider에서.
-//            throw new BaseException(POST_USERS_EXISTS_EMAIL);
-//        }
-//
-//        String pwd;
-//        try{
-//            //암호화
-//            pwd = new SHA256().encrypt(postMemberReq.getPassword());
-//            postMemberReq.setPassword(pwd);
-//        } catch (Exception ignored) {
-//            throw new BaseException(PASSWORD_ENCRYPTION_ERROR);
-//        }
-//        try{
-//            int userIdx = memerDao.createUser(postMemberReq);
-//            //jwt 발급.
-//            // TODO: jwt는 다음주차에서 배울 내용입니다!
-//            String jwt = jwtService.createJwt(userIdx);
-//            return new PostMemberRes(jwt,userIdx);
-//        } catch (Exception exception) {
-//            exception.printStackTrace();
-//            throw new BaseException(DATABASE_ERROR);
-//        }
-//    }
-//
-//    public void modifyUserName(PatchMemberReq patchMemberReq) throws BaseException {
-//        try{
-//            int result = memerDao.modifyMemberFirstName(patchMemberReq);
-//            if(result == 0){
-//                throw new BaseException(MODIFY_FAIL_USERNAME);
-//            }
-//        } catch(Exception exception){
-//            throw new BaseException(DATABASE_ERROR);
-//        }
-//    }
-
 }
