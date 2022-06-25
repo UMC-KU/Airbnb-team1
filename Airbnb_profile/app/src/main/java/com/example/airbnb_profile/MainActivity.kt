87f4fb64e79661fb23d4d99bedb3e39bf35500fa
dlawltn0708 <dlawotn3@naver.com>
2022-06-25 16:25:19 +0900

    Feat bottom_navigation&Profile

--- Airbnb_profile/app/src/main/java/com/example/airbnb_profile/MainActivity.kt
+++ Airbnb_profile/app/src/main/java/com/example/airbnb_profile/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.airbnb_profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.airbnb_profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btn = findViewById<Button>(R.id.btn)
+
+//        btn.setOnClickListener {
+//            val profilefragment = ProfileFragment()
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(R.id.f,profilefragment)
+//                .commit()
+//        }
+
+
     }
 }
